1fe5ea6eb2b4aa7fffe35d48eb2bd3b8120699ac
Naman Agrawal <naman@innovation.nj.gov>
2023-07-19 20:04:17 -0400

    Update scripts to allow utils

--- importNJWDS.sh
+++ importNJWDS.sh
@@ -1,2 +1,8 @@
+echo "Installing Node dependencies..."
 npm install
-mkdir -p assets/njwds/dist && cp -r ./node_modules/@newjersey/njwds/dist/* ./assets/njwds/dist
+
+echo "Creating njwds/dist directory..."
+mkdir -p assets/njwds/dist
+
+echo "Copying NJWDS files from npm package into project..."
+cp -r ./node_modules/@newjersey/njwds/dist/* ./assets/njwds/dist
