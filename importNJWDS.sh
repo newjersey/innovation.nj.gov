c9a9be8e6da7e216f6544597a52830b0273a2cdc
Naman Agrawal <naman@innovation.nj.gov>
2023-07-26 19:22:08 -0400

    Remove assets before pulling new ones

--- importNJWDS.sh
+++ importNJWDS.sh
@@ -1,6 +1,9 @@
 echo "Installing Node dependencies..."
 npm install
 
+echo "Deleting njwds/dist directory if it exists..."
+rm -rf assets/njwds/dist
+
 echo "Creating njwds/dist directory..."
 mkdir -p assets/njwds/dist
 
