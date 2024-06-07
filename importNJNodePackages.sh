7dcd8198e95c20e2d29aa85345b96153094932e5
Noah Marcus <noah@innovation.nj.gov>
2024-06-07 17:15:19 -0400

    added feedback widget to site (design incomplete)

---
+++ importNJNodePackages.sh
@@ -0,0 +1,20 @@
+echo "Installing Node dependencies..."
+npm install
+
+echo "Deleting njwds/dist directory if it exists..."
+rm -rf assets/njwds/dist
+
+echo "Deleting feedback-widget/ directory if it exists..."
+rm -rf assets/feedback-widget
+
+echo "Creating njwds/dist directory..."
+mkdir -p assets/njwds/dist
+
+echo "Creating feedback-widget/ directory..."
+mkdir -p assets/feedback-widget
+
+echo "Copying NJWDS files from npm package into project..."
+cp -r ./node_modules/@newjersey/njwds/dist/* ./assets/njwds/dist
+
+echo "Copying NJ Feedback Widget files from npm package into project..."
+cp -r ./node_modules/@newjersey/feedback-widget/feedback-widget.min.js ./assets/feedback-widget/
